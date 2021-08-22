c9ffbb8e378d855349d5a934de02ae8950d76b89
ohdair <ohdiar@gmail.com>
2021-08-22 10:25:18 +0900

    jaewpark

---
+++ test.java
@@ -0,0 +1,10 @@
+import packages.first.Student;
+
+public class test {
+
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.studentname = "gilee";
+        System.out.println(s.getName());
+    }
+}
